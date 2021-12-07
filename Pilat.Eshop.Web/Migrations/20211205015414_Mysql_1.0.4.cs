a3f8f96eb14ce1eee2d0f3f93d8c5c556a60fee1
kpilat <58290934+kpilat@users.noreply.github.com>
2021-12-07 18:44:37 +0100

    Related products add

---
+++ Pilat.Eshop.Web/Migrations/20211205015414_Mysql_1.0.4.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pilat.Eshop.Web.Migrations
+{
+    public partial class Mysql_104 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Related",
+                table: "Products",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Related",
+                table: "Products");
+        }
+    }
+}
---
+++ Pilat.Eshop.Web/Migrations/20211205135148_Mysql_1.0.5.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pilat.Eshop.Web.Migrations
+{
+    public partial class Mysql_105 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
